d0f76e46bb3bc67070b1b2ff14be6d08aacdf34d
LainaJ <lainakarosic@gmail.com>
2022-02-21 10:03:15 -0500

    nft-project

---
+++ ethereum/scripts/deploy.js
@@ -0,0 +1,14 @@
+async function main() {
+    const { ethers } = require('hardhat');
+    const EmotionalShapes = await ethers.getContractFactory("EmotionalShapes");
+    const emotionalShapes = await EmotionalShapes.deploy();
+  
+    console.log("EmotionalShapes deployed:", emotionalShapes.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
